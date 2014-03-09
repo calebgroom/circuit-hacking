13556bc0d4df159db17a3fbee57a833806dc9aa4
Caleb Groom <caleb@calebgroom.com>
2014-03-09 19:49:14 +0000

    Cleaning up blink_led.py

--- raspberrypi/blink_led.py
+++ raspberrypi/blink_led.py
@@ -1,16 +1,27 @@
 """Blink a LED on GPIO on an interval."""
-
+import argparse
 import time
+
 import RPi.GPIO as GPIO
 
-INTERVAL = 1
-PIN = 25
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(PIN, GPIO.OUT)
+def main():
+    """Main script execution."""
+    parser = argparse.ArgumentParser(description='LED light blinking script.')
+    parser.add_argument('--pin', type=int, default=25, help='GPIO pin number')
+    parser.add_argument('--interval', type=int, default=1,
+                        help='Time in seconds to blink')
+    args = parser.parse_args()
 
-while True:
-    GPIO.output(PIN, GPIO.HIGH)
-    time.sleep(INTERVAL)
-    GPIO.output(PIN, GPIO.LOW)
-    time.sleep(INTERVAL)
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(args.pin, GPIO.OUT)
+
+    while True:
+        GPIO.output(args.pin, GPIO.HIGH)
+        time.sleep(args.interval)
+        GPIO.output(args.pin, GPIO.LOW)
+        time.sleep(args.interval)
+
+if __name__ == '__main__':
+    main()
